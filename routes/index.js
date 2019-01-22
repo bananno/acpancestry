a87f5c3c895201a6ac32a3cc3de5c7d6abfa9a67
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-21 16:38:18 -0800

    Begin adding main routes

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,24 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/people', function(req, res, next) {
+  res.render('index', {
+    database: database,
+  });
+});
+
+router.get('/events', function(req, res, next) {
+  res.render('index', {
+    database: database,
+  });
+});
+
+router.get('/sources', function(req, res, next) {
+  res.render('index', {
+    database: database,
+  });
+});
+
 router.get('/person/:personId', function(req, res, next) {
   let personId = req.params.personId;
   let currentPerson = database.people.filter(nextPerson => {
