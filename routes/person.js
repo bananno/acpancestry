f4b295aa3d8676aa3528c34175fbf7f592520582
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-23 12:32:05 -0800

    Add an error page for invalid person ID

--- routes/person.js
+++ routes/person.js
@@ -8,13 +8,26 @@ router.get('/:personId', function(req, res, next) {
   let currentPerson = database.people.filter(nextPerson => {
     return nextPerson.customId == personId;
   })[0];
-  res.render('layout', {
-    view: 'people/layout',
-    subview: 'summary',
-    title: currentPerson.name,
-    database: database,
-    person: currentPerson,
-  });
+
+  if (currentPerson && currentPerson.length) {
+    res.render('layout', {
+      view: 'people/layout',
+      subview: 'summary',
+      title: currentPerson.name,
+      database: database,
+      person: currentPerson,
+    });
+  } else {
+    res.render('layout', {
+      view: 'people/layout',
+      subview: 'notFound',
+      title: personId,
+      database: database,
+      person: {
+        name: personId,
+      },
+    });
+  }
 });
 
 module.exports = router;
