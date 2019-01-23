605c4af3c94b6d3d80c1249f2a0f37e4fd03b674
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-23 07:50:39 -0800

    Process database to reference people directly instead of IDs

--- database/data.js
+++ database/data.js
@@ -1,4 +1,19 @@
 
 const data = require('./raw.js');
+let relationshipTypes = ['parents', 'spouses', 'children'];
+
+data.peopleRef = {};
+
+data.people.forEach(person => {
+  data.peopleRef[person._id] = person;
+});
+
+data.people.forEach(person => {
+  relationshipTypes.forEach(rel => {
+    person[rel] = person[rel].map(relative => {
+      return data.peopleRef[relative] || null;
+    })
+  });
+});
 
 module.exports = data;
