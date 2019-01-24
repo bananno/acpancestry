a2bd69d50457032f0d547fca1c0671bddb1a20b4
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-24 14:05:46 -0800

    Include people by name

--- tools/searchResults.js
+++ tools/searchResults.js
@@ -1,10 +1,34 @@
 
 const database = require('../database/data.js');
 
+let searchArr = [];
+
+const processSearchInput = (searchStr) => {
+  searchArr = searchStr.toLowerCase().split(' ');
+}
+
+const includePerson = (person) => {
+  let tempName = person.name.toLowerCase();
+
+  for (let i in searchArr) {
+    if (!tempName.match(searchArr[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const includeEvent = (event) => {
+  return false;
+}
+
 const getSearchResults = (searchStr) => {
+  processSearchInput(searchStr);
+
   return {
-    people: [],
-    events: [],
+    people: database.people.filter(includePerson),
+    events: database.events.filter(includeEvent),
     sources: [],
   };
 };
