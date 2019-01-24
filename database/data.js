35d5dc8ba9d1174d8351dcfaf4b7fa29ba2db541
Anna Peterson <peterson.anna.c@gmail.com>
2019-01-23 16:51:52 -0800

    Store list of events for each person

--- database/data.js
+++ database/data.js
@@ -13,6 +13,7 @@ data.people.forEach(person => {
     person[rel] = person[rel].map(relative => data.peopleRef[relative] || null)
     person[rel] = person[rel].filter(relative => relative != null);
   });
+  person.events = [];
 });
 
 data.events.forEach(event => {
@@ -21,11 +22,17 @@ data.events.forEach(event => {
     if (person == null || person.private) {
       return null;
     }
+    person.events.push(event);
     return person;
   });
   event.people = event.people.filter(person => person != null);
 });
 
 data.events = data.events.filter(event => event.people.length > 0);
+
+data.people.forEach(person => {
+  person.birth = person.events.filter(event => event.title == 'birth')[0];
+  person.death = person.events.filter(event => event.title == 'death')[0];
+});
 
 module.exports = data;
