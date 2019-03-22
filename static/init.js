e38577227a432486115bf47015f8aa0c01a615fe
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-22 10:14:43 -0700

    Create header links based on actual url

--- static/init.js
+++ static/init.js
@@ -1,12 +1,24 @@
 
-const ORIGIN = (() => {
+const ORIGIN = getOrigin();
+
+$(document).ready(() => {
+  createHeaderLinks();
+});
+
+function getOrigin() {
   let url = window.location.href;
 
   if (url.match('\\?')) {
-    return url.slice(0, url.indexOf('\\?'));
+    return url.slice(0, url.indexOf('\?'));
   }
 
   return url;
-})();
+}
 
-console.log(ORIGIN);
+function createHeaderLinks() {
+  $('#page-header h1').append('<a href="' + ORIGIN + '">Lundberg Ancestry</a>');
+  $('#main-navigation').append('<li><a href="' + ORIGIN + '">Home</a></li>');
+  $('#main-navigation').append('<li><a href="' + ORIGIN + '?people">People</a></li>');
+  $('#main-navigation').append('<li><a href="' + ORIGIN + '?events">Events</a></li>');
+  $('#main-navigation').append('<li><a href="' + ORIGIN + '?sources">Sources</a></li>');
+}
