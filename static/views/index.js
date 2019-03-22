88bc156ed3af3c7a1351dcf9c7ee0871f4e42b0b
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-22 15:20:56 -0700

    Show list of people based on search results

--- static/views/index.js
+++ static/views/index.js
@@ -51,6 +51,17 @@ function viewSources() {
 function viewSearch() {
   setPageTitle('Search Results');
   rend('<h1>Search Results</h1>');
-  const keywords = PATH.slice(7).split('+').filter(word => word.length > 0);
+  const keywords = PATH.slice(7).toLowerCase().split('+').filter(word => word.length > 0);
   $('.search-form [name="search"]').val(keywords.join(' '));
+
+  let peopleList = DATABASE.people.filter(person => {
+    for (let i = 0; i < keywords.length; i++) {
+      if (person.name.toLowerCase().match(keywords[i]) == null) {
+        return false;
+      }
+    }
+    return true;
+  });
+
+  rend($makePeopleList(peopleList));
 }
