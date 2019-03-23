dc064673ca1c73b5fd66130c91198a4ea8ec822e
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-23 10:55:39 -0700

    Build mobile version of navigation menu

--- static/init.js
+++ static/init.js
@@ -5,6 +5,7 @@ $(document).ready(() => {
   processDatabase();
   createHeaderLinks();
   loadContent();
+  toggleMobileMenu();
 });
 
 function getFilePaths() {
@@ -21,8 +22,19 @@ function getFilePaths() {
 
 function createHeaderLinks() {
   $('#page-header h1').append('<a href="' + ORIGIN + '">Lundberg Ancestry</a>');
-  $('#main-navigation').append('<li><a href="' + ORIGIN + '">Home</a></li>');
-  $('#main-navigation').append('<li><a href="' + ORIGIN + '?people">People</a></li>');
-  $('#main-navigation').append('<li><a href="' + ORIGIN + '?events">Events</a></li>');
-  $('#main-navigation').append('<li><a href="' + ORIGIN + '?sources">Sources</a></li>');
+  const $list = $('#main-navigation ul');
+  $list.append('<li><a href="' + ORIGIN + '">Home</a></li>');
+  $list.append('<li><a href="' + ORIGIN + '?people">People</a></li>');
+  $list.append('<li><a href="' + ORIGIN + '?events">Events</a></li>');
+  $list.append('<li><a href="' + ORIGIN + '?sources">Sources</a></li>');
+}
+
+function toggleMobileMenu() {
+  $('#menu-icon').click(() => {
+    $('#main-navigation').css('width', '255px');
+  });
+
+  $('#main-navigation .close-me').click(() => {
+    $('#main-navigation').css('width', '0');
+  });
 }
