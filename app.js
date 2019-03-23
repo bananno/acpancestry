b6ceb230d18718dd72b7597f329c703946d72c5d
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-23 11:17:11 -0700

    Change express public directory to allow both verions of app to share files

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '')));
 
 app.use('/', indexRouter);
 app.use('/person', personRouter);
