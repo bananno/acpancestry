bc8be2399ab92a1bda896fb4f4a268d4d871f5c1
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-23 18:35:59 -0700

    Fix problem where static html file loads instead of route

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ app.set('view engine', 'ejs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '')));
+
+// Static files are in the public directory. Second line means that "public/" must be included
+// in the path. This prevents loading the static index.html instead of using "/" route.
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static('public'))
 
 app.use('/', indexRouter);
 app.use('/person', personRouter);
