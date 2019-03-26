306bb27fcde67c1695f5f4202540b677b5aed146
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-26 15:35:53 -0700

    Show table with basic source info

--- static/views/sources.js
+++ static/views/sources.js
@@ -1,5 +1,21 @@
 
 function viewSources() {
   setPageTitle('Sources');
+  const $table = $('<table class="event-list" border="1">');
+
   rend('<h1>All Sources</h1>');
+  rend($table);
+
+  $table.append($headerRow(['type', 'group', 'title', 'date', 'location', 'people']));
+
+  DATABASE.sources.forEach(source => {
+    const $row = $('<tr>').appendTo($table);
+
+    addTd($row, source.type);
+    addTd($row, source.group);
+    addTd($row, source.title);
+    addTd($row, formatDate(source.date));
+    addTd($row, formatLocation(source.location));
+    addTd($row, $makePeopleList(source.people));
+  });
 }
