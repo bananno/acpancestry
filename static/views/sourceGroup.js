db319a2c70867545a31c0aab179f04a8440e57f6
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-26 17:31:27 -0700

    Add view for cemetery group

--- static/views/sourceGroup.js
+++ static/views/sourceGroup.js
@@ -8,4 +8,27 @@ function viewSourceGroup() {
     rend('<h1>Source not found</h1>');
     return;
   }
+
+  if (source.type == 'grave') {
+    viewSourceGroupCemetery(source);
+    return;
+  }
+
+  rend('<h1>Source group</h1>');
+}
+
+function viewSourceGroupCemetery(rootSource) {
+  const cemeteryName = rootSource.group;
+
+  setPageTitle(cemeteryName);
+  rend('<h1>' + cemeteryName + '</h1>');
+  rend('<p style="padding-top: 10px;">' + formatLocation(rootSource.location) + '</p>');
+
+  DATABASE.sources.forEach(source => {
+    if (source.type != 'grave' || source.group != cemeteryName) {
+      return;
+    }
+
+    rend('<h2>' + source.title + '</h2>');
+  });
 }
