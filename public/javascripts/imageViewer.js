3c53f8aa0d8c50746088c0f3bb65f4eb21c5c020
Anna Peterson <peterson.anna.c@gmail.com>
2019-03-28 14:22:42 -0700

    Remove image viewer functionality and use hyperlink instead

--- public/javascripts/imageViewer.js
+++ public/javascripts/imageViewer.js
@@ -1,36 +1,20 @@
 
 function makeImage(url, maxHeight, maxWidth) {
-  const $imageViewer = $('<div class="image-viewer">');
-
-  const $thumbnail = $('<div class="image-thumbnail">').appendTo($imageViewer);
-  const $popup = $('<div class="image-popup">').appendTo($imageViewer);
-  const $inset = $('<div class="image-inset">').appendTo($popup);
-
-  $thumbnail.append('<img src="' + url + '">');
-  $thumbnail.append('click to enlarge');
-
-  $inset.append('<div>Close</div>');
-  $inset.append('<img src="' + url + '">');
+  const $imageViewer = $(
+    '<div class="image-viewer">' +
+      '<a href="' + url + '" target=_"blank"><img src="' + url + '">click to enlarge</a>' +
+    '</div>'
+  );
 
   if (maxHeight) {
-    $thumbnail.find('img').css('max-height', maxHeight + 'px');
+    $imageViewer.find('img').css('max-height', maxHeight + 'px');
   }
 
   if (maxWidth) {
-    $thumbnail.find('img').css('max-width', maxWidth + 'px');
+    $imageViewer.find('img').css('max-width', maxWidth + 'px');
+  } else {
+    $imageViewer.find('img').css('max-width', '100%');
   }
-
-  $thumbnail.click(() => {
-    $popup.show();
-  });
-
-  $popup.click(() => {
-    $popup.hide();
-  });
-
-  $popup.find('div').click(() => {
-    $popup.hide();
-  });
 
   return $imageViewer;
 }
