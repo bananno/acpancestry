ad6b679953a7726b6ec559771d1d683fbedd889f
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-02 23:01:21 -0700

    Combine newspaper and cemetery group functions

--- static/sources/group.js
+++ static/sources/group.js
@@ -10,84 +10,46 @@ function viewSourceGroup() {
   }
 
   if (source.type == 'grave') {
-    viewSourceGroupCemetery(source);
+    viewSourceGroupCemeteryOrNewspaper(source, 'Cemeteries');
     return;
   }
 
   if (source.type == 'newspaper') {
-    viewSourceGroupNewspaper(source);
+    viewSourceGroupCemeteryOrNewspaper(source, 'Newspapers', true);
     return;
   }
 
   rend('<h1>Source group</h1>');
 }
 
-function viewSourceGroupCemetery(rootSource) {
-  const cemeteryName = rootSource.group;
-  setPageTitle(cemeteryName);
+function viewSourceGroupCemeteryOrNewspaper(rootSource, group, showDate) {
+  const groupName = rootSource.group;
+  setPageTitle(groupName);
 
   rend(
     '<p style="margin-bottom: 10px; font-size: 15px;">' +
       '<u>' + localLink('sources', 'Sources') + '</u>' +
       ' &#8594; ' +
-      '<u>' + localLink('sources/cemeteries', 'Cemeteries') + '</u>' +
+      '<u>' + localLink('sources/' + group.toLowerCase(), group) + '</u>' +
     '</p>'
   );
 
-  rend('<h1>' + cemeteryName + '</h1>');
+  rend('<h1>' + groupName + '</h1>');
   rend('<p style="padding-top: 10px;">' + formatLocation(rootSource.location) + '</p>');
 
   DATABASE.sources.forEach(source => {
-    if (source.type != 'grave' || source.group != cemeteryName) {
+    if (source.type != rootSource.type || source.group != groupName) {
       return;
     }
 
     rend('<h2>' + source.title + '</h2>');
+
+    if (showDate && source.date.format) {
+      rend('<p style="margin-left: 10px; margin-bottom: 10px;">' + source.date.format + '</p>');
+    }
 
     source.images.forEach(imageUrl => {
       rend(makeImage(imageUrl, 100, 100).css('margin', '0 5px'));
-    });
-
-    rend($makePeopleList(source.people, 'photo'));
-
-    if (source.content) {
-      rend(formatTranscription(source.content));
-    }
-
-    if (source.notes) {
-      rend('<p>' + source.notes + '</p>');
-    }
-
-    source.links.forEach(linkUrl => {
-      rend(getFancyLink(linkUrl));
-    });
-  });
-}
-
-function viewSourceGroupNewspaper(rootSource) {
-  const newspaperName = rootSource.group;
-  setPageTitle(newspaperName);
-
-  rend(
-    '<p style="margin-bottom: 10px; font-size: 15px;">' +
-      '<u>' + localLink('sources', 'Sources') + '</u>' +
-      ' &#8594; ' +
-      '<u>' + localLink('sources/newspapers', 'Newspapers') + '</u>' +
-    '</p>'
-  );
-
-  rend('<h1>' + newspaperName + '</h1>');
-  rend('<p style="padding-top: 10px;">' + formatLocation(rootSource.location) + '</p>');
-
-  DATABASE.sources.forEach(source => {
-    if (source.type != 'newspaper' || source.group != newspaperName) {
-      return;
-    }
-
-    rend('<h2>' + source.title + '</h2>');
-
-    source.images.forEach(imageUrl => {
-      rend(makeImage(imageUrl, 100, 100));
     });
 
     rend($makePeopleList(source.people, 'photo'));
