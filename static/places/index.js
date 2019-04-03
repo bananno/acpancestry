864699fece8269309a64e43a23a18c0ef88a1392
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-03 14:41:28 -0700

    Show list of countries

--- static/places/index.js
+++ static/places/index.js
@@ -2,4 +2,27 @@
 function viewPlaces() {
   setPageTitle('Places');
   rend('<h1>Places</h1>');
+
+  const countryList = [];
+  const listByCountry = {};
+  listByCountry['Not Specified'] = [];
+
+  [...DATABASE.events, ...DATABASE.sources].forEach(item => {
+    let country = item.location.country;
+
+    if (country) {
+      if (!listByCountry[country]) {
+        countryList.push(country);
+        listByCountry[country] = [];
+      }
+    } else {
+      country = 'Not Specified';
+    }
+
+    listByCountry[country].push(item);
+  });
+
+  countryList.forEach(country => {
+    rend('<p>' + country + ' (' + listByCountry[country].length + ')</p>');
+  });
 }
