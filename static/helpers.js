24646b2ef5e7f98ac0f2ac96584d7db85a728ab8
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-03 14:58:09 -0700

    Move constants to own file; fix typo in constant name

--- static/helpers.js
+++ static/helpers.js
@@ -1,19 +1,3 @@
-const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-  'August', 'September', 'October', 'November', 'December'];
-
-const USE_STATES = {
-    AL: 'Alabama', AK: 'Alaska', AZ: 'Arizona', AR: 'Arkansas', CA: 'California',
-    CO: 'Colorado', CT: 'Connecticut', DE: 'Delaware', FL: 'Florida', GA: 'Georgia',
-    HI: 'Hawaii', ID: 'Idaho', IL: 'Illinois', IN: 'Indiana', IA: 'Iowa',
-    KS: 'Kansas', KY: 'Kentucky', LA: 'Louisiana', ME: 'Maine', MD: 'Maryland',
-    MA: 'Massachusetts', MI: 'Michigan', MN: 'Minnesota', MS: 'Mississippi', MO: 'Missouri',
-    MT: 'Montana', NE: 'Nebraska', NV: 'Nevada', NH: 'New Hampshire', NJ: 'New Jersey',
-    NM: 'New Mexico', NY: 'New York', NC: 'North Carolina', ND: 'North Dakota', OH: 'Ohio',
-    OK: 'Oklahoma', OR: 'Oregon', PA: 'Pennsylvania', RI: 'Rhode Island', SC: 'South Carolina',
-    SD: 'South Dakota', TN: 'Tennessee', TX: 'Texas', UT: 'Utah', VT: 'Vermont',
-    VA: 'Virginia', WA: 'Washington', WV: 'West Virginia', WI: 'Wisconsin', WY: 'Wyoming',
-};
-
 function setPageTitle(title) {
   if (title && title.length) {
     document.title = title + ' | Lundberg Ancestry';
@@ -126,7 +110,7 @@ function formatLocation(locat) {
 
   if (isUSA) {
     if (locat.region1) {
-      locationComponents.push(USE_STATES[locat.region1] || locat.region1);
+      locationComponents.push(USA_STATES[locat.region1] || locat.region1);
     }
     locationComponents.push('USA');
   } else {
