ceb172711a4b58e41afd1af2f011273b1dc98ab0
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-04 22:51:19 -0700

    Format events and sources for place view

--- static/places/items.js
+++ static/places/items.js
@@ -1,6 +1,51 @@
 
-function viewPlacesItemList(items) {
-  items.forEach(item => {
-    rend('<p>' + item.title + '</p>');
+function viewPlacesItemList(itemList) {
+  [['Cemeteries', 'grave', 'grave'],
+  ['Newspapers', 'newspaper', 'article']].forEach(([sectionTitle, sourceType, entryName]) => {
+    const groupList = createListOfNewspapersOrCemeteries(sourceType, itemList);
+    let needHeader = true;
+
+    for (let groupName in groupList) {
+      if (needHeader) {
+        rend('<h2>' + sectionTitle + '</h2>');
+        needHeader = false;
+      }
+
+      const rootSource = groupList[groupName][0];
+      const numItems = groupList[groupName].length;
+
+      rend(sourceListitemCemeteryOrNewspaper(rootSource, entryName, numItems, true));
+    }
   });
+
+  const otherItems = itemList.filter(item => item.type != 'grave' && item.type != 'newspaper');
+
+  if (otherItems.length) {
+    rend('<h2>Other</h2>');
+  }
+
+  otherItems.forEach(item => {
+    if (item.group) {
+      viewPlacesItemSource(item);
+    } else {
+      viewPlacesItemEvent(item);
+    }
+  });
+}
+
+function viewPlacesItemSource(source) {
+  rend(
+    '<p style="margin-top: 10px;">' +
+    linkToSource(source, source.type + ' - ' + source.group + ' - ' + source.title) +
+    '</p>'
+  );
+}
+
+function viewPlacesItemEvent(event) {
+  rend(
+    '<p style="margin-top: 10px;">' +
+      event.title + ' - ' +
+      event.people.map(person => linkToPerson(person)).join(', ') +
+    '</p>'
+  );
 }
