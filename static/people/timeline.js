661bc1ba8065233e466ddad1379faf22ebf9c80a
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-05 19:58:00 -0700

    Add sources and events to timeline

--- static/people/timeline.js
+++ static/people/timeline.js
@@ -4,13 +4,28 @@ function showPersonTimeline(person) {
 
   const items = getPersonTimelineItems(person);
 
-  items.forEach(item => {
-    rend('<p>' + item.title + '</p>');
-  });
+  items.forEach(showPersonTimelineItem);
 }
 
 function getPersonTimelineItems(person) {
-  return [...DATABASE.events, ...DATABASE.sources].filter(item => {
+  const list = [...DATABASE.events, ...DATABASE.sources].filter(item => {
     return item.people.indexOf(person) >= 0;
   });
+
+  return list;
+}
+
+function showPersonTimelineItem(item) {
+  rend(
+    '<p style="margin-left: 15px; margin-top: 10px; padding-top: 10px; border-top: 1px solid black;">' +
+      [item.type,
+      item.group,
+      item.title,
+      item.date.format,
+      item.location.format,
+      item.people ? item.people.map(person => person.name).join(', ') : '',
+      item.location.note
+      ].filter(attr => attr).join('<br>') +
+    '</p>'
+  );
 }
