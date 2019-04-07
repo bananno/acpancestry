86a8f82a59f7a3615be8581e2dae5afdd18772c3
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-06 21:36:32 -0700

    Format timeline

--- static/people/timeline.js
+++ static/people/timeline.js
@@ -8,26 +8,40 @@ function showPersonTimeline(person) {
 }
 
 function getPersonTimelineItems(person) {
-  const list = [...DATABASE.events, ...DATABASE.sources].filter(item => {
-    return item.people.indexOf(person) >= 0;
+  const list = [];
+
+  DATABASE.events.forEach(item => {
+    if (item.people.indexOf(person) >= 0) {
+      let newItem = {...item};
+      newItem.event = true;
+      list.push(newItem);
+    }
+  });
+
+  DATABASE.sources.forEach(item => {
+    if (item.people.indexOf(person) >= 0) {
+      let newItem = {...item};
+      newItem.source = true;
+      list.push(newItem);
+    }
   });
 
   const dateParts = ['year', 'month', 'day'];
 
-  list.sort((item2, item1) => {
+  list.sort((item1, item2) => {
     for (let i = 0; i < 3; i++) {
       const datePart1 = item1.date[dateParts[i]];
       const datePart2 = item2.date[dateParts[i]];
 
-      if (datePart2 == null) {
+      if (datePart1 == null) {
         return 0;
       }
 
-      if (datePart1 == null) {
+      if (datePart2 == null) {
         return -1;
       }
 
-      if (datePart1 == datePart2) {
+      if (datePart2 != datePart1) {
         return datePart1 - datePart2;
       }
     }
@@ -41,30 +55,47 @@ function showPersonTimelineItem(item) {
   const $div = $('<div class="timeline-item">');
   rend($div);
 
-  const $col1 = $('<div class="timeline-item-date-place">').appendTo($div);
-  const $col2 = $('<div>').appendTo($div);
+  const $col1 = $('<div class="column timeline-item-date-place">').appendTo($div);
+  const $col2 = $('<div class="column">').appendTo($div);
+
+  if (item.event && (item.title == 'birth' || item.title == 'death')) {
+    $div.addClass('main-event');
+  }
 
   if (item.date.format) {
     $col1.append('<p><b>' + item.date.format + '</b></p>');
+  } else if ($('.timeline-no-date').length == 0 && item.type != 'grave') {
+    $div.before('<div class="timeline-no-date">No date:</div>')
   }
 
   if (item.location.format) {
     $col1.append('<p>' + item.location.format + '</p>');
   }
 
-  if (item.location.note) {
-    $col1.append('<p><i>(' + item.location.note + ')</i></p>');
+  if (item.location.notes) {
+    $col1.append('<p><i>(' + item.location.notes + ')</i></p>');
   }
 
-  if (item.type) {
-    $col2.append('<p>' + item.group + ' - ' + item.title + '</p>');
+  if (item.source) {
+    if (item.type == 'index') {
+      $col2.append('<p><b>source</b></p>');
+    } else if (item.type == 'grave') {
+      $col2.append('<p><b>cemetery</b></p>');
+    } else {
+      $col2.append('<p><b>' + item.type + '</b></p>');
+    }
+    $col2.append('<p>' + linkToSource(item, item.group + ' - ' + item.title) + '</p>');
   } else {
     $col2.append('<p><b>' + item.title + '</b></p>');
   }
 
-  if (item.note) {
-    $col2.append('<p>' + item.note + '</p>');
+  if (item.notes) {
+    $col2.append('<p>' + item.notes + '</p>');
   }
 
-  $col2.append($makePeopleList(item.people, 'photo'));
+  if (item.type == 'photo' && item.images.length) {
+    $col1.append(makeImage(item.images[0], 100));
+  }
+
+  $col2.append($makePeopleList(item.people, 'photo').css('margin-left', '-5px'));
 }
