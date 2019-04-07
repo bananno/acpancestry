4156b0827f0075aa0b5c9b4feab80110d5f01584
Anna Peterson <peterson.anna.c@gmail.com>
2019-04-06 19:54:15 -0700

    Format person timeline items

--- static/people/timeline.js
+++ static/people/timeline.js
@@ -12,20 +12,51 @@ function getPersonTimelineItems(person) {
     return item.people.indexOf(person) >= 0;
   });
 
+  const dateParts = ['year', 'month', 'day'];
+
+  list.sort((item2, item1) => {
+    for (let i = 0; i < 3; i++) {
+      const datePart1 = item1.date[dateParts[i]];
+      const datePart2 = item2.date[dateParts[i]];
+
+      if (datePart2 == null) {
+        return 0;
+      }
+
+      if (datePart1 == null) {
+        return -1;
+      }
+
+      if (datePart1 == datePart2) {
+        return datePart1 - datePart2;
+      }
+    }
+    return 0;
+  });
+
   return list;
 }
 
 function showPersonTimelineItem(item) {
-  rend(
-    '<p style="margin-left: 15px; margin-top: 10px; padding-top: 10px; border-top: 1px solid black;">' +
-      [item.type,
-      item.group,
-      item.title,
-      item.date.format,
-      item.location.format,
-      item.people ? item.people.map(person => person.name).join(', ') : '',
-      item.location.note
-      ].filter(attr => attr).join('<br>') +
-    '</p>'
+  const $div = $('<div class="timeline-item">');
+  rend($div);
+
+  $div.append(
+    '<div class="timeline-item-date-place">' +
+      (item.date.format ? '<b>' + item.date.format + '</b>' : '') +
+      (item.date.format && item.location.format ? '<br>' : '') +
+      item.location.format +
+    '</div>'
+  );
+
+  const $col2 = $('<div>').appendTo($div);
+
+  $col2.append(
+    [item.type,
+    item.group,
+    item.title,
+    item.people ? item.people.map(person => person.name).join(', ') : '',
+    item.location.note
+    ].filter(attr => attr).join('<br>')
   );
 }
