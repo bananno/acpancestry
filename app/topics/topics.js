cedf41da1ce646c86c5494dafa6e85864fdcfd80
Anna Peterson <peterson.anna.c@gmail.com>
2019-08-15 19:26:26 -0700

    Move topics to subdirectory

---
+++ app/topics/topics.js
@@ -0,0 +1,81 @@
+
+function viewTopic() {
+  const topic = PATH.replace('topic/', '');
+
+  if (topic == 'military') {
+    return viewTopicMilitary();
+  }
+
+  if (topic == 'immigration') {
+    return viewTopicImmigration();
+  }
+
+  if (topic == 'disease') {
+    return viewTopicDisease();
+  }
+
+  return pageNotFound();
+}
+
+function viewTopicImmigration() {
+  const countries = [];
+  const peopleByCountry = {};
+
+  const people = DATABASE.people.filter(person => person.tags.immigrant);
+
+  people.forEach(person => {
+    (person.tags.country || 'Other').split(',').forEach(country => {
+      if (!countries.includes(country)) {
+        countries.push(country);
+        peopleByCountry[country] = [];
+      }
+      peopleByCountry[country].push(person);
+    });
+  });
+
+  countries.trueSort((a, b) => a < b && a != 'Other');
+
+  setPageTitle('Immigration');
+  h1('Immigration');
+
+  countries.forEach(country => {
+    h2(country);
+    rend($makePeopleList(peopleByCountry[country], 'photo'));
+  });
+
+  h2('Timeline');
+
+  new Timeline(null, null, {
+    sourceFilter: source => source.tags.immigration,
+    eventFilter: event => event.title == 'immigration' || event.tags.immigration,
+    sort: true,
+    render: true
+  });
+}
+
+function viewTopicDisease() {
+  const people = DATABASE.people.filter(person => person.tags['died of disease']);
+
+  const events = DATABASE.events.filter(event => {
+    if (event.title.match('illness')) {
+      return true;
+    }
+    if (event.title == 'death' && people.map(person => person._id).includes(event.people[0]._id)) {
+      return true;
+    }
+    return false;
+  });
+
+  events.trueSort((a, b) => a.date.sort < b.date.sort);
+
+  setPageTitle('Disease');
+  h1('Disease');
+
+  h2('People that died of disease');
+  rend($makePeopleList(people, 'photo'));
+
+  h2('Events');
+  events.forEach(event => {
+    rend(eventBlock(event));
+  });
+}
