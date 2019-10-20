9def1b468c1c10d7029196f4a1f6b763947a4a0a
Anna Peterson <peterson.anna.c@gmail.com>
2019-10-20 16:38:49 -0700

    Update cemetery/newspaper search to remove old source group functionality

--- app/search/cemeteriesNewspapers.js
+++ app/search/cemeteriesNewspapers.js
@@ -1,91 +1,108 @@
 class SearchResultsCemeteriesOrNewspapers extends SearchResults {
-  constructor(keywords, isTest, sourceType, groupTitle, entryTitle, entrySingular) {
+  constructor(keywords, isTest, options) {
     super(keywords, isTest);
-    this.sourceType = sourceType;
-    this.groupTitle = groupTitle;
-    this.entryTitle = entryTitle;
-    this.entrySingular = entrySingular;
-    this.groupList = [];
-    this.groupEntryCount = {};
-    this.individualList = [];
+
+    this.storyType = options.storyType;
+    this.sectionStoryTitle = options.sectionStoryTitle;
+    this.sectionSourceTitle = options.sectionSourceTitle;
+    this.entryName = options.entryName;
+
+    this.resultsListStories = [];
+    this.resultsListSources = [];
+
     this.getResults();
     this.renderGroupResults();
     this.renderIndividualResults();
   }
 
   getResults() {
-    DATABASE.sources.forEach(source => {
-      if (source.type != this.sourceType) {
+    DATABASE.stories.forEach(story => {
+      if (story.type != this.storyType) {
         return;
       }
 
-      if (this.isMatch(source.group)) {
-        if (this.groupEntryCount[source.group]) {
-          this.groupEntryCount[source.group] += 1;
-        } else {
-          this.groupEntryCount[source.group] = 1;
-          this.groupList.push(source);
+      if (this.isMatch(story.title)) {
+        this.resultsListStories.push(story);
+      }
+
+      story.entries.forEach(source => {
+        let searchString = story.title + source.title + source.content;
+
+        if (this.isMatch(searchString)) {
+          this.resultsListSources.push(source);
         }
-      }
-
-      let searchString = source.title + source.content;
-
-      if (this.isMatch(searchString)) {
-        this.individualList.push(source);
-      }
+      });
     });
   }
 
   renderGroupResults() {
-    if (this.groupList.length == 0) {
+    if (this.resultsListStories.length == 0) {
       return;
     }
 
-    this.title(this.groupTitle);
+    this.title(this.sectionStoryTitle);
 
-    this.groupList.forEach(source => {
-      let linkText = this.highlight(source.group);
+    this.resultsListStories.forEach(story => {
+      let linkText = this.highlight(story.title);
       rend(
         '<p style="padding: 5px 10px" class="search-result-item">' +
-          linkToSourceGroup(source, linkText) + '<br>' +
-          source.location.format + '<br>' +
-          this.groupEntryCount[source.group] + ' ' + this.entrySingular +
-          (this.groupEntryCount[source.group] == 1 ? '' : 's') +
+          linkToStory(story, linkText) + '<br>' +
+          story.location.format + '<br>' +
+          story.entries.length + ' ' +
+          this.entryName.pluralize(story.entries.length) +
         '</p>'
       );
     });
   }
 
   renderIndividualResults() {
-    if (this.individualList.length == 0 ) {
+    if (this.resultsListSources.length == 0 ) {
       return;
     }
 
-    this.title(this.entryTitle);
+    this.title(this.sectionSourceTitle);
 
-    this.individualList.forEach(source => {
-      rend(
-        '<p style="padding: 5px 10px" class="search-result-item">' +
-          linkToSource(source, this.highlight(source.title)) + '<br>' +
-          source.group + '<br>' +
-          (source.date.format ? source.date.format + '<br>' : '') +
-          (source.location.format ? source.location.format + '<br>' : '') +
-        '</p>'
-      );
+    this.resultsListSources.forEach((source, i) => {
+      pg(linkToSource(source, this.highlight(source.title)))
+        .css('padding', '2px 10px')
+        .css('padding-top', (i == 0 ? '5px' : '15px'))
+        .addClass('search-result-item');
 
-      rend(formatTranscription(this.highlight(source.content)));
+      pg(source.story.title).css('padding', '2px 10px');
+
+      ['date', 'location'].forEach(attr => {
+        if (source[attr].format) {
+          pg(source[attr].format).css('padding', '2px 10px');
+        } else if (source.story[attr].format) {
+          pg(source.story[attr].format).css('padding', '2px 10px');
+        }
+      });
+
+      if (source.content) {
+        rend(formatTranscription(this.highlight(source.content)));
+      }
     });
   }
 }
 
 class SearchResultsCemeteries extends SearchResultsCemeteriesOrNewspapers {
   constructor(keywords, isTest) {
-    super(keywords, isTest, 'newspaper', 'Newspapers', 'Newspaper Articles', 'article');
+    super(keywords, isTest, {
+      storyType: 'newspaper',
+      sectionStoryTitle: 'Newspapers',
+      sectionSourceTitle: 'Newspaper Articles',
+      entryName: 'article'
+    });
   }
 }
 
 class SearchResultsNewspapers extends SearchResultsCemeteriesOrNewspapers {
   constructor(keywords, isTest) {
-    super(keywords, isTest, 'grave', 'Cemeteries', 'Graves', 'grave');
+    super(keywords, isTest, {
+      storyType: 'cemetery',
+      sectionStoryTitle: 'Cemeteries',
+      sectionSourceTitle: 'Graves',
+      entryName: 'grave'
+    });
   }
 }
